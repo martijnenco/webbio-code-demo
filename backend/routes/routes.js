f13e257edd2b4943744a46097d88b75f98dea4b3
martijnenco <m-j-heiden@hotmail.com>
2019-09-12 11:31:44 +0200

    Clean backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,6 @@
-// app/routes.js
 const partnersController = require('../controllers/partnersController');
 
 module.exports = {
-  _io: null,
-
   setRoutes(app) {
     app.get('/api/partners', partnersController);
   },
